05beecfc2d8de41d614c2b1c1f3979cb29837b1c
kiemsoatngaptnm-web <kiemsoatngaptnm@gmail.com>
2025-09-20 08:33:37 +0700

    Add files via upload

--- proxy.py
+++ proxy.py
@@ -48,4 +48,5 @@ def get_data():
     return jsonify(r.json())
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
