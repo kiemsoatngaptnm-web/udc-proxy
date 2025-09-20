5155fcced0f5ef53cc85bdc1e44b4c1ae53e889e
kiemsoatngaptnm-web <kiemsoatngaptnm@gmail.com>
2025-09-20 08:48:57 +0700

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -50,3 +51,4 @@ def get_data():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
