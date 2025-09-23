6aeaf237d55298bff069ab1a12c6127b2a5760a0
kiemsoatngaptnm-web <kiemsoatngaptnm@gmail.com>
2025-09-23 08:54:12 +0700

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -129,7 +129,7 @@ def udc_data():
 
         #     result[name] = values
 
-        return jsonify(result)
+        return jsonify(data)
 
         # data = resp.json()
         # result = {}
@@ -361,6 +361,7 @@ if __name__ == "__main__":
 
 # if __name__ == "__main__":
 #     app.run(host="0.0.0.0", port=5000, debug=True)
+
 
 
 
